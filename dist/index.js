ce9eb1d90ac8d1a1601c4ac73afb4fbc67801d1f
Abdulkbk <abdoollahikbk@gmail.com>
2022-12-05 16:48:11 +0100

    connected prisma with postgresql

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,70 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("dotenv/config");
+const express_1 = __importStar(require("express"));
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
+app.use((0, express_1.urlencoded)({ extended: false }));
+app.use((0, express_1.json)());
 const port = process.env.PORT || 8000;
 app.get('/', (req, res) => res.send('Hello from express server!'));
+app.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name, email, posts } = req.body;
+    const postData = posts === null || posts === void 0 ? void 0 : posts.map((post) => {
+        return { title: post === null || post === void 0 ? void 0 : post.title, content: post === null || post === void 0 ? void 0 : post.content };
+    });
+    const result = yield prisma.user.create({
+        data: {
+            name,
+            email,
+            posts: {
+                create: postData,
+            },
+        },
+    });
+    res.json(result);
+}));
+app.post(`/post`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, content, authorEmail } = req.body;
+    const result = yield prisma.post.create({
+        data: {
+            title,
+            content,
+            author: { connect: { email: authorEmail } },
+        },
+    });
+    res.json(result);
+}));
 app.listen(port, () => console.log(`Server started at port ${port}`));
--- index.ts
+++ index.ts
@@ -1,12 +1,48 @@
-import express, { Express, Request, Response } from 'express'
-import 'dotenv/config'
+import express, { Express, Request, Response, urlencoded, json } from 'express'
+import { Prisma, PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 const app: Express = express()
+
+app.use(urlencoded({ extended: false }))
+app.use(json())
 
 const port = process.env.PORT || 8000
 
 app.get('/', (req: Request, res: Response) =>
   res.send('Hello from express server!')
 )
+
+app.post('/signup', async (req: Request, res: Response) => {
+  const { name, email, posts } = req.body
+
+  const postData = posts?.map((post: Prisma.PostCreateInput) => {
+    return { title: post?.title, content: post?.content }
+  })
+
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email,
+      posts: {
+        create: postData,
+      },
+    },
+  })
+
+  res.json(result)
+})
+
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+  })
+  res.json(result)
+})
 
 app.listen(port, () => console.log(`Server started at port ${port}`))
